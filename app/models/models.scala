1bebef67d06e886d2dc820fada57ca37ec8e9c90
Sébastien renault <sre@zenexity.com>
2013-04-11 00:23:16 +0200

    rename app i18n package

--- app/models/models.scala
+++ app/models/models.scala
@@ -5,6 +5,6 @@ import org.xnap.commons.i18n.I18n
 import org.xnap.commons.i18n.I18nFactory
 
 object I18n {
-  lazy val fr = I18nFactory.getI18n(getClass, "wio.i18n.Messages_fr", java.util.Locale.FRENCH)
-  lazy val en = I18nFactory.getI18n(getClass, "wio.i18n.Messages_en", java.util.Locale.ENGLISH)
+  lazy val fr = I18nFactory.getI18n(getClass, "playgettext.i18n.Messages_fr", java.util.Locale.FRENCH)
+  lazy val en = I18nFactory.getI18n(getClass, "playgettext.i18n.Messages_en", java.util.Locale.ENGLISH)
 }
--- updateMessages.sh
+++ updateMessages.sh
@@ -1,2 +1,2 @@
-msgfmt --java -d target/scala-2.10/classes -r wio.i18n.Messages -l fr conf/po/fr.po
-msgfmt --java -d target/scala-2.10/classes -r wio.i18n.Messages -l en conf/po/en.po
+msgfmt --java -d target/scala-2.10/classes -r playgettext.i18n.Messages -l fr conf/po/fr.po
+msgfmt --java -d target/scala-2.10/classes -r playgettext.i18n.Messages -l en conf/po/en.po
