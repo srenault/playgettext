5e0abc7622edb3d8738e8a452da5647e0fe40ea8
Sébastien renault <sre@zenexity.com>
2013-04-11 15:50:15 +0200

    Manage header

--- app/models/parser.scala
+++ app/models/parser.scala
@@ -6,8 +6,10 @@ import scala.util.parsing.combinator._
 import scala.util.matching._
 
 case class Message(key: String, pattern: String, input: scalax.io.Input, sourceName: String) extends Positional
+case class Comment(msg: String)
 
 class MessagesParser(messageInput: scalax.io.Input, messageSourceName: String) extends RegexParsers {
+
   override def skipWhitespace = false
 
   def namedError[A](p: Parser[A], msg: String) = Parser[A] { i =>
@@ -17,9 +19,15 @@ class MessagesParser(messageInput: scalax.io.Input, messageSourceName: String) e
     }
   }
 
-  def end = namedError("""\s*""".r, "End of line expected")
+  def end = namedError("""\s*""".r, "End of file expected")
 
   def newLine = namedError((("\r"?) ~> "\n"), "End of line expected")
+
+  def blankLine = opt(whiteSpace) ~> newLine ^^ { case _ => Comment("") }
+
+  def header = """".[^"]+"""".r ^^ { case _ => Comment("") }
+
+  def comment = """#.*""".r ^^ { case s => Comment(s) }
 
   def quote = namedError("\"", "Quote expected")
 
@@ -33,7 +41,9 @@ class MessagesParser(messageInput: scalax.io.Input, messageSourceName: String) e
     }
   }
 
-  def parser = phrase((message*) <~ end) ^^ {
+  def sentence = (header | comment | positioned(message))
+
+  def parser = phrase((sentence | newLine)*) ^^ {
     case messages => messages.collect {
       case m @ Message(_, _, _, _) => m
     }
@@ -42,7 +52,14 @@ class MessagesParser(messageInput: scalax.io.Input, messageSourceName: String) e
   def parse = {
     parser(new CharSequenceReader(messageInput.string + "\n")) match {
       case Success(messages, _) => messages
-      case NoSuccess(message, in) => throw new Exception(message)
+      case NoSuccess(message, in) => {
+        throw new play.api.PlayException.ExceptionSource("Configuration error", message) {
+          def line = in.pos.line
+          def position = in.pos.column - 1
+          def input = messageInput.string
+          def sourceName = messageSourceName
+        }
+      }
     }
   }
 }
