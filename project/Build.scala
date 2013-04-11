43d1cdfe8ebe2a14e40060e9e9125e74271b08c1
Sébastien renault <sre@zenexity.com>
2013-04-11 09:43:49 +0200

    Create a sbt task to generate Ressource Bundle classes

--- project/Build.scala
+++ project/Build.scala
@@ -17,8 +17,24 @@ object ApplicationBuild extends Build {
   val gettext = TaskKey[Unit]("gettext", "Generate Ressource Bundle classes from PO files for i18n")
 
   val gettextTask = gettext <<= (baseDirectory, streams) map { (base, s) =>
-    s.log.info("Generating Ressource Bundle classes from the following PO files :")
-    s.log.info(IO.listFiles(base / "conf" / "po").map(_.getName:String).toList.mkString("\n"))
+    import scala.sys.process._
+
+    s.log.info("Generating Ressource Bundle classes from :")
+    val poDirectory = (base / "conf" / "po")
+    val poFiles = IO.listFiles(poDirectory)
+    s.log.info(poFiles.map(_.getName:String).mkString("\n"))
+
+    val targetDirectory = (base / "target" / "scala-2.10" / "classes").getCanonicalPath
+    poFiles foreach { po =>
+      val poPath = po.getCanonicalPath
+      val lang = po.getName.split("\\.").headOption
+      lang.map { lg =>
+        val cmd = "msgfmt --java -d %s -r playgettext.i18n.Messages -l %s %s".format(targetDirectory, lg, poPath)
+        if((Process(cmd) ! ProcessLogger(out => (), err => s.log.error(err))) != 0) sys.error("Can't generate Ressource Bundle Class for: " + po.getCanonicalPath)
+      } getOrElse {
+        s.log.warn("The following PO file is ignored: " + po.getCanonicalPath)
+      }
+    }
   }
 
   val main = play.Project(appName, appVersion, appDependencies, settings = Seq(gettextTask) ++ Defaults.defaultSettings).settings(
